8d3d176a51c6e629cacd39eff0159c475b760690
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2022-09-22 22:36:18 +0200

    Add migration script

---
+++ src/scripts/migration.py
@@ -0,0 +1,13 @@
+from ast import main
+from tkinter.tix import MAIN
+import redis
+
+if __name__ == '__main__':
+    client = redis.Redis(host='localhost', port=6379)
+
+    for key in client.scan_iter('*'):
+        server_id = key.decode().split('#')[0]
+        meme = client.hgetall(key)
+        new_key = 'meme:' + server_id + ':' + (meme[b'name'].decode()).lower()
+        client.hmset(new_key, meme)
+        print('Imported: ', new_key)
