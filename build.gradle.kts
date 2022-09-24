c9ea5200ecebcefa834cbe9502ff7f927d93811c
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2022-09-24 17:19:35 +0200

    Cleanup vaadin conf

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.13.RELEASE"
 	id("jacoco")
 	id("org.sonarqube") version "3.4.0.2513"
-//	id("com.vaadin") version "23.1.7"
 	war
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
@@ -23,13 +22,11 @@ repositories {
 	}
 }
 
-//extra["vaadinVersion"] = "23.1.7"
 
 dependencies {
 	//implementation("org.springframework.boot:spring-boot-starter-amqp")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	//implementation("com.vaadin:vaadin-spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("net.dv8tion:JDA:4.4.0_352")
@@ -38,7 +35,7 @@ dependencies {
 	implementation("commons-validator:commons-validator:1.7")
 	testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.amqp:spring-rabbit-test")
+	//testImplementation("org.springframework.amqp:spring-rabbit-test")
 	testImplementation("it.ozimov:embedded-redis:0.7.3") {
 		exclude(group = "org.slf4j", module = "slf4j-simple")
 	}
@@ -48,11 +45,6 @@ dependencies {
 	implementation("io.github.microutils:kotlin-logging-jvm:3.0.0")
 }
 
-dependencyManagement {
-	imports {
-//		mavenBom("com.vaadin:vaadin-bom:${property("vaadinVersion")}")
-	}
-}
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
